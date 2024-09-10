067b35aafcd9f7683c211d8f8957f97bb7a16e19
Callum Young <callum.young@triad.co.uk>
2024-09-10 14:24:09 +0100

    base test

--- terraform/azure.tf
+++ terraform/azure.tf
@@ -1,24 +1,51 @@
+
 terraform {
+
+  backend "azurerm" {
+    key                  = "github.terraform.tfstate"
+  }
+
+  required_version = ">=0.12"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.54"
-    }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~>2.43"
+      version = "~>2.0"
     }
   }
-
-  backend "azurerm" {}
 }
+
+##
+# Providers
+##
 
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
-  alias           = "sub"
-  subscription_id = var.subscription_id
+  features {}
 }
+
+
+
+#terraform {
+#  required_providers {
+#    azurerm = {
+#      source  = "hashicorp/azurerm"
+#      version = "~>3.54"
+#    }
+#    azuread = {
+#      source  = "hashicorp/azuread"
+#      version = "~>2.43"
+#    }
+#  }
+#
+#  backend "azurerm" {}
+#}
+#
+#provider "azurerm" {
+#  features {
+#    resource_group {
+#      prevent_deletion_if_contains_resources = false
+#    }
+#  }
+#  alias           = "sub"
+#  subscription_id = var.subscription_id
+#}
