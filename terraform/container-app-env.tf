bb78eaeae5256afd83b9fca394ac5672d67ee0d0
Callum <callum1197@gmail.com>
2024-10-31 00:01:14 +0000

    test

--- terraform/container-app-env.tf
+++ terraform/container-app-env.tf
@@ -7,39 +7,17 @@
 #  #log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
 #}
 
-resource "azurerm_app_service_environment" "managed_environment" {
-    name                = "tf-created-cae"
-    resource_group_name = azurerm_resource_group.main_rg.name
-    location            = var.location
+resource "azurerm_container_app_environment" "example" {
+  name                = "test-tf-cae"
+  resource_group_name = "Manual-cae-test"
+  location            = var.location
+  internal           = true
 
-  #zone_redundant = false 
-    subnet_id = data.azurerm_subnet.this.id
-
-  #infrastructure_resource_group = "ME_managedEnvironment-tf"
-  #infrastructure_subnet_id   = data.azurerm_subnet.this.id
-
-
-  #vnet_configuration {
-  #  internal                   = true
-  #  infrastructure_subnet_id   = data.azurerm_subnet.this.id
-  #}
-
-  #peer_authentication {
-  #  mtls {
-  #    enabled = false
-  #  }
-  #}
-
-  #peer_traffic_configuration {
-  #  encryption {
-  #    enabled = false
-  #  }
-  #}
-
-    workload_profile {
-        workload_profile_type = "Consumption"
-        name                  = "Consumption"
-    }
+  # Set the VNet configuration
+  vnet_configuration {
+    subnet_id = azurerm_subnet.example.id
+  }
+  
 }
 
 data "azurerm_virtual_network" "this" {
