49639ece47084c0ef70101e3e1d6b80a012ed62c
Callum <callum1197@gmail.com>
2024-10-31 00:18:02 +0000

    test

--- terraform/azure.tf
+++ terraform/azure.tf
@@ -1,50 +1,30 @@
 
 terraform {
+
   backend "azurerm" {
     key                  = "github.terraform.tfstate"
   }
+
+  required_version = ">=0.12"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "~>2.0"
     }
   }
 }
 
-# Configure the Microsoft Azure Provider
-#provider "azurerm" {
-#  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-#  features {}
-#}
+##
+# Providers
+##
+
+provider "azurerm" {
+  features {}
+}
 
 
 
-#terraform {
-#
-#  backend "azurerm" {
-#    key                  = "github.terraform.tfstate"
-#  }
-#
-#  required_version = ">=0.12"
-#
-#  required_providers {
-#    azurerm = {
-#      source  = "hashicorp/azurerm"
-#      version = "~>4.7.6"
-#    }
-#  }
-#}
-#
-###
-## Providers
-###
-#
-#provider "azurerm" {
-#  features {}
-#}
-#
-#
-#
 #terraform {
 #  required_providers {
 #    azurerm = {
