bca2603b7d3f12397af4b6b1267f055a2ef4577f
Callum <callum1197@gmail.com>
2024-10-31 12:23:29 +0000

    remove asp

--- terraform/app-service-plan.tf
+++ terraform/app-service-plan.tf
@@ -1,11 +1,11 @@
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-appserviceplan"
-  location            = azurerm_resource_group.main_rg.location
-  resource_group_name = azurerm_resource_group.main_rg.name
-  #os_type             = "Linux." 
-
-  sku {
-    tier = "PremiumV2"
-    size = "P1v2"
-  }
-}
+#resource "azurerm_app_service_plan" "example" {
+#  name                = "example-appserviceplan"
+#  location            = azurerm_resource_group.main_rg.location
+#  resource_group_name = azurerm_resource_group.main_rg.name
+#  #os_type             = "Linux." 
+#
+#  sku {
+#    tier = "PremiumV2"
+#    size = "P1v2"
+#  }
+#}
