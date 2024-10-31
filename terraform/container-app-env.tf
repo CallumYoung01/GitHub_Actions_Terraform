280783abb07d9e904b36cb315e769676dbbaaed4
Callum <callum1197@gmail.com>
2024-10-31 11:26:08 +0000

    update

--- terraform/container-app-env.tf
+++ terraform/container-app-env.tf
@@ -1,24 +1,14 @@
-
-
-#resource "azurerm_container_app_environment" "example" {
-#  name                       = "my-environment"
-#  location                   = var.location
-#  resource_group_name        = azurerm_resource_group.main_rg.name
-#  #log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
-#}
-
-resource "azurerm_container_app_environment" "test" {
-  name                = "test-tf-cae"
+# Create a Container App Environment
+resource "azurerm_container_app_environment" "example" {
+  name                = "cae-tf-test"
   resource_group_name = "Manual-cae-test"
   location            = var.location
-  internal           = true
-
-  # Set the VNet configuration
-  vnet_configuration {
-    subnet_id = azurerm_subnet.example.id
-  }
-
+  infrastructure_subnet_id = data.azurerm_subnet.this.id
+  internal_load_balancer_enabled = true
+  # No internal or vnet_configuration here
 }
+
+
 
 data "azurerm_virtual_network" "this" {
     name = "manutal-vnet-1"
